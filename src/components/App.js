5c6b32cc63ede2151dfbc17a94ba39bae304e611
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-18 11:15:17 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [value, setValue] = useState("");
+
+  useEffect(function () {
+    console.log(value);
+  });
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>App</h1>
+      <input type="text" onChange={handleChange} value={value} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
